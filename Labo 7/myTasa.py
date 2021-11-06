383c27ed369838ce97d75c7d44b7e9f4490c371e
ayalatom <tomas.1808@hotmail.com>
2021-11-05 22:41:29 -0300

    Mismo formato que en ../Moneda/tasa_moneda.py

--- Labo 7/myTasa.py
+++ Labo 7/myTasa.py
@@ -1,15 +1,15 @@
 import os
 import numpy as np
 import cv2
-import datetime
 
-path = '/home/tom/Escritorio/Data/Calibración/'
-video = '65_74'
+path = os.path.expanduser('~/Escritorio/Data/Calibración/')
+#path = os.path.expanduser('~/Escritorio/Data/Experimentos/')
+file = '65_74'
 
 tasa = 1 # Voy a guardar uno de cada 'tasa' frames
 
 radio = 150 # Radio de la arena (en milímetros)
-step = 1000 # Duración del giro (en milisegundos)
+time = 1000 # Tiempo de un giro (en milisegundos)
 left, right = 65, 74 # Calibración del Kilobot
 
 r_ar = 356 # Guess para el radio de la arena (¡Verificar en KolourPaint!)
@@ -17,46 +17,46 @@ r_oj = 6 # Guess para el radio de los ojalillos
 e = 16 # 2*e va a ser el lado del recorte cuadrado
 th = 13 # Va a ser el param2 (un umbral) del método HoughCircles
 
-archivo_video  = path + video + '.mp4'
-carpeta_frames = path + video + '/frames/'
-archivo_param  = path + video + '/' + video + '_param.csv'
+#################### CARGO EL VIDEO Y CREO SU CARPETA ####################
+video_fname = path + file + '.mp4'
+frames_dir = path + file + '/frames/'
 
-################################ CARGO EL VIDEO Y CREO SU CARPETA ################################
-cap = cv2.VideoCapture(archivo_video)
+cap = cv2.VideoCapture(video_fname)
 
 try:
-    os.makedirs(carpeta_frames)
-    print("Se creó '%s'\n" % carpeta_frames)
+    os.makedirs(frames_dir)
+    print("Se creó '%s'\n" % frames_dir)
 except FileExistsError:
-    print("Ya existe '%s'\n" % carpeta_frames)
+    print("Ya existe '%s'\n" % frames_dir)
 
-################################ EXTRAIGO LOS FRAMES ################################
-fps = int(cap.get(cv2.CAP_PROP_FPS))
-count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-length = datetime.timedelta(seconds=count/fps)
+#################### EXTRAIGO LOS FRAMES ####################
+fps = cap.get(cv2.CAP_PROP_FPS)
+frames_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+segundos = frames_count / fps
 
-print("Este video '%s.mp4' (grabado a %d fps) dura %s y tiene %d frames\n" 
-    % (video, fps, str(length).split('.')[0], count))
-print('Extraigo frames con una tasa = %d...' % tasa)
+print("Este video '%s.mp4', grabado a %d fps, tiene %d frames y dura %d min." 
+      % (file, fps, frames_count, segundos/60))
+print('Extraigo 1 de cada %d frames...' % tasa)
 
 i = 1
-
 while cap.isOpened():
     ret, frame = cap.read()
     if ret == False:
         break
     if i%tasa == 0:
-        cv2.imwrite(carpeta_frames + 'frame_' + str(i) + '.jpg', frame)
-        if i%1200 == 0:
-            tiempo_parcial = datetime.timedelta(seconds=i/fps)
-            print('...%s completo (frame %d)' % (str(tiempo_parcial), i))
+        frame_fname = frames_dir + 'frame_' + str(i) + '.jpg'
+        cv2.imwrite(frame_fname, frame)
+    if i%1000 == 0:
+        pc = (i/frames_count) * 100
+        print('...%d%% completo' % pc)
     i+=1
 
 cap.release()
 cv2.destroyAllWindows()
 
-################################ GUARDO LOS PARÁMETROS COMO .CSV ################################
-PARAM = np.array([[0, 0, 0, 0, r_ar, r_oj, e, th, tasa, radio, step, fps, count, left, right]])
-my_params = 'i_left,j_left,i_right,j_right,r_ar,r_oj,e,th,tasa,radio,step,fps,count,left,right'
+#################### EXPORTO LOS METADATOS COMO .CSV ####################
+meta = np.array([[0, 0, 0, 0, r_ar, r_oj, e, th, tasa, radio, time, left, right, fps, frames_count, segundos]])
+meta_fname = path + file + '/' + file + '_meta.csv'
+meta_header = 'i_left,j_left,i_right,j_right,r_ar,r_oj,e,th,tasa,radio,time,left,right,fps,frames_count,segundos'
 
-np.savetxt(archivo_param, PARAM, fmt='%d', delimiter=',', comments='', header=my_params)
+np.savetxt(meta_fname, meta, header=meta_header, fmt='%d', delimiter=',', comments='')
